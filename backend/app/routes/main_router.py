34bb67fec203dfe6e3eaa94ceefff7bc817712a2
peep12ng <peep12ng@gmail.com>
2023-05-23 17:46:59 +0900

    nice

--- backend/app/routes/main_router.py
+++ backend/app/routes/main_router.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, make_response
 from ..api import Collecter, riotapi
 
 from ..models import MatchDetail
@@ -24,3 +24,33 @@ def main():
     data = {"result": result}
 
     return jsonify(data)
+
+@bp.route("/test", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def test():
+    if request.method == 'POST':
+        print('POST')
+        data = request.get_json()
+        print(data)
+        print(data['value'])
+    if request.method == 'GET':
+        print('GET')
+        user = request.args.get('name')
+        print(user)
+    if request.method == 'PUT':
+        print('PUT')
+        user = request.args.get('value')
+        print(user)
+    if request.method == 'DELETE':
+        print('DELETE')
+        user = request.args.get('value')
+        print(user)
+
+    return make_response(jsonify({'status': True}), 200)
+
+@bp.route("/search", methods=['GET'])
+def search():
+    if request.method == 'GET':
+        user = request.args.get('name')
+        print(user)
+
+    return make_response(jsonify({'status': True}), 200)
--- frontend/frontend/src/components/Test.vue
+++ frontend/frontend/src/components/Test.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
-        <p>{{ msg }}</p>
+        <button type="button" class="btn btn-primary" v-on:click="test">TEST</button>
+        <button type="button" class="btn btn-primary" v-on:click="test_get">GET</button>
+        <button type="button" class="btn btn-primary" v-on:click="test_post">POST</button>
+        <button type="button" class="btn btn-primary" v-on:click="test_put">PUT</button>
+        <button type="button" class="btn btn-primary" v-on:click="test_delete">DELETE</button>
+    </div>
+    <div>
+        <form v-on:click="search">
+            <div>
+                <label for="name">name:</label>
+                <input id="name" type="text" v-model="name">
+            </div>
+            <button type="button">search</button>
+            <p>{{ name }}</p>
+        </form>
     </div>
 </template>
 
@@ -8,27 +22,83 @@
 import axios from 'axios';
 
 export default {
-    name: 'Test',
     data() {
         return {
-            msg: 'aa',
-        };
+            name: '',
+        }
     },
     methods: {
-        getMessage() {
-            const path = 'http://127.0.0.1:5000/main';
-            axios.get(path)
-                .then((res) => {
-                    this.msg = res.data;
-                })
-                .catch((error) => {
-
-                    console.error(error);
-                });
+        search: function() {
+            console.log(this.name)
+            axios('http://127.0.0.1:5000/search', {
+                method: "get",
+                params: {
+                    name: this.name,
+                },
+            })
+            .then(function(response){
+                console.log(response);
+            })
+            .catch(function(error){
+                console.log(error);
+            });
         },
-    },
-    created() {
-        this.getMessage();
-    },
+        test_get: () => {
+            axios('http://127.0.0.1:5000/test', {
+                method: "get",
+                params: {
+                    name: "test_get",
+                },
+            })
+            .then((response) => {
+                console.log(response.data["status"]);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        },
+        test_post: () => {
+            axios('http://127.0.0.1:5000/test', {
+                method: "post",
+                data: {
+                    value: "test_post",
+                },
+            })
+            .then((response) => {
+                console.log(response.data["status"]);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        },
+        test_put: () => {
+            axios('http://127.0.0.1:5000/test', {
+                method: "put",
+                params: {
+                    value: "test_put",
+                },
+            })
+            .then((response) => {
+                console.log(response.data["status"]);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        },
+        test_delete: () => {
+            axios('http://127.0.0.1:5000/test', {
+                method: "delete",
+                params: {
+                    value: "test_delete",
+                },
+            })
+            .then((response) => {
+                console.log(response.data["status"]);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        },
+    }
 };
 </script>
